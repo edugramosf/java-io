492cd25c59e72b7c9a318e37d78b06550d811c76
Eduardo Ramos <edugramosf@gmail.com>
2022-06-12 09:47:53 -0300

    Teste Formatação com Scanner.

--- src/br/com/alura/java/io/teste/TesteScanner.java
+++ src/br/com/alura/java/io/teste/TesteScanner.java
@@ -1,8 +1,6 @@
 package br.com.alura.java.io.teste;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -19,13 +17,13 @@ public class TesteScanner {
             linhascanner.useLocale(Locale.US);
             linhascanner.useDelimiter(",");
 
-            String valor1 = linhascanner.next();
-            int valor2 = linhascanner.nextInt();
-            int valor3 = linhascanner.nextInt();
-            String valor4 = linhascanner.next();
-            double valor5 = linhascanner.nextDouble();
+            String tipoConta = linhascanner.next();
+            int agencia = linhascanner.nextInt();
+            int numero = linhascanner.nextInt();
+            String titular = linhascanner.next();
+            double saldo = linhascanner.nextDouble();
 
-            String valorFormatado = String.format("%s - %s-%s, %s: %s", valor1, valor2, valor3, valor4, valor5);
+            String valorFormatado = String.format("%s - %04d-%08d, %s: %05.2f", tipoConta, agencia, numero, titular, saldo);
             System.out.println(valorFormatado);
 
             linhascanner.close();
