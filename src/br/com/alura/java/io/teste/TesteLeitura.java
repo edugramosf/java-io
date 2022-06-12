6b9342ffad9ee312baa0b831ed7f8906a8d50663
Eduardo Ramos <edugramosf@gmail.com>
2022-06-12 04:24:15 -0300

    Teste de Escrita

--- src/br/com/alura/java/io/teste/TesteLeitura.java
+++ src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -7,8 +7,8 @@ public class TesteLeitura {
     public static void main(String[] args) throws IOException {
 
 //        Fluxo de Entrada com Arquivo
-        FileInputStream fis = new FileInputStream("lorem.txt");
-        InputStreamReader isr = new InputStreamReader(fis);
+        InputStream fis = new FileInputStream("lorem.txt");
+        Reader isr = new InputStreamReader(fis);
         BufferedReader br = new BufferedReader(isr);
 
         String linha = br.readLine();
