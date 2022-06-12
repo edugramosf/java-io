49945fd2bb620dd87e2b163b26bdaf458964c88d
Eduardo Ramos <edugramosf@gmail.com>
2022-06-12 06:49:08 -0300

    Teste de Escrita com FileWriter.

---
+++ src/br/com/alura/java/io/teste/TesteEscritaFileWriter.java
@@ -0,0 +1,25 @@
+package br.com.alura.java.io.teste;
+
+import java.io.*;
+
+public class TesteEscritaFileWriter {
+
+    public static void main(String[] args) throws IOException {
+
+//        Fluxo de Entrada com Arquivo
+//        OutputStream fos = new FileOutputStream("lorem2.txt");
+//        Writer osw = new OutputStreamWriter(fos);
+//        BufferedWriter bw = new BufferedWriter(osw);
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter("Lorem4.txt"));
+        bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+        bw.newLine();
+        bw.newLine();
+        bw.newLine();
+        bw.write("asfasdfsadas dfs sdf asf assdB");
+
+        bw.close();
+
+    }
+
+}
